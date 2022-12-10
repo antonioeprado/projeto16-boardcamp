ae24a7eaf33081f89d18cf287955179d090c6104
Antonio Prado <antonioeprado18@gmail.com>
2022-12-10 14:16:02 -0300

    feat: validation model

---
+++ src/models/validateModel.js
@@ -0,0 +1,39 @@
+import { categoryModel } from "./categories.models.js";
+import { customerModel } from "./customers.models.js";
+import { gameModel } from "./games.models.js";
+
+export default class ValidateModel {
+	constructor(obj, modelToValidate) {
+		this.obj = obj;
+		this.modelToValidate = modelToValidate;
+		this.models = { categoryModel, customerModel, gameModel };
+	}
+	get result() {
+		return this.setModel();
+	}
+	setModel() {
+		switch (this.modelToValidate) {
+			case "customers":
+				return this.validateModel(this.models.customerModel, this.obj);
+			case "games":
+				return this.validateModel(this.models.gameModel, this.obj);
+			case "category":
+				return this.validateModel(this.models.categoryModel, this.obj);
+			default:
+				return this.validateModel();
+		}
+	}
+
+	validateModel(model, obj) {
+		if (!model) {
+			return "No model was provided.";
+		}
+		const { value, error } = model.validate(obj);
+		if (error) {
+			const errors = error.details.map((detail) => detail.message);
+			console.log(errors);
+			return { errors, status: false };
+		}
+		return { value, status: true };
+	}
+}
