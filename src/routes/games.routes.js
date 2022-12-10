45fc1c16e6390d5e878ff4a3513fd725f6e706ea
Antonio Prado <antonioeprado18@gmail.com>
2022-12-10 14:22:17 -0300

    feat: games route complete

--- src/routes/games.routes.js
+++ src/routes/games.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { getGames, postGames } from "../controllers/games.controllers.js";
+import {
+	isInDatabase,
+	validatePayload,
+} from "../middlewares/games.middlewares.js";
 
 const router = Router();
 
 router.get("/games", getGames);
-router.post("/games/", postGames);
+router.post("/games/", validatePayload, isInDatabase, postGames);
 
 export default router;
