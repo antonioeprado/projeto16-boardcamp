9678494f3fcf08b1cd49c0e018c23888b7cdec53
Antonio Prado <antonioeprado18@gmail.com>
2022-12-10 14:17:10 -0300

    feat: categories route complete

---
+++ src/middlewares/categories.middlewares.js
@@ -0,0 +1,22 @@
+import { connection } from "../database/database.js";
+import ValidateModel from "../models/validateModel.js";
+
+export const validatePayload = (req, res, next) => {
+	const validation = new ValidateModel(req.body, "category");
+	const { status, ...rest } = validation.result;
+	if (!status) {
+		return res.status(400).send(rest);
+	}
+	next();
+};
+
+export const isInDatabase = async (req, res, next) => {
+	const query = await connection.query(
+		"SELECT * FROM categories WHERE name=$1",
+		[req.body.name]
+	);
+	if (query.rowCount) {
+		return res.sendStatus(409);
+	}
+	next();
+};
--- src/routes/categories.routes.js
+++ src/routes/categories.routes.js
@@ -3,10 +3,14 @@ import {
 	getCategories,
 	postCategories,
 } from "../controllers/categories.controllers.js";
+import {
+	isInDatabase,
+	validatePayload,
+} from "../middlewares/categories.middlewares.js";
 
 const router = Router();
 
 router.get("/categories", getCategories);
-router.post("/categories", postCategories);
+router.post("/categories", validatePayload, isInDatabase, postCategories);
 
 export default router;
